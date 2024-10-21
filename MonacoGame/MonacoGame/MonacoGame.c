3ee38796a1dcd1c440501ea438853cc491c6f4d9
ghabryeu <gabiconstantino2001@gmail.com>
2024-10-21 17:31:02 -0300

    Menu + personagem

--- MonacoGame/MonacoGame/MonacoGame.c
+++ MonacoGame/MonacoGame/MonacoGame.c
@@ -1,186 +1,109 @@
 #include <allegro5/allegro.h>
+#include <allegro5/allegro_font.h>
 #include <allegro5/allegro_image.h>
-#include <assert.h>
+#include <allegro5/allegro_primitives.h>
 
 
-typedef struct {
-    int pos_x, pos_y;
-    int vel;
-    ALLEGRO_BITMAP* direita;
-    ALLEGRO_BITMAP* esquerda;
-    ALLEGRO_BITMAP* atual;
-    bool facing_left;
-}Personagem;
-
-typedef struct {
-    ALLEGRO_DISPLAY* display;
-    ALLEGRO_EVENT_QUEUE* queue;
-    ALLEGRO_TIMER* timer;
-    bool running;
-}Jogo;
-
-typedef struct {
-    ALLEGRO_BITMAP* img_bloco;
-    ALLEGRO_BITMAP* img_grama;
-}Cenario;
-
-ALLEGRO_BITMAP* carregar_imagem_cenario(char img[]) {
-    ALLEGRO_BITMAP* bitmap = al_load_bitmap(img);
-    assert(bitmap);
-
-    return bitmap;
-}
-
-ALLEGRO_BITMAP* carregar_imagem_redimensionada(char img[], int tamanho) {
-    ALLEGRO_BITMAP* bitmap = al_load_bitmap(img);
-    assert(bitmap);
-
-    int original_width = al_get_bitmap_width(bitmap);
-    int original_height = al_get_bitmap_height(bitmap);
-
-    int new_width = original_width / tamanho;
-    int new_height = original_height / tamanho;
-
-    // mexendo no tamanho da imagem
-    ALLEGRO_BITMAP* new_bitmap = al_create_bitmap(new_width, new_height);
-    al_set_target_bitmap(new_bitmap); // local dos prÃ­ximos desenhos
-    al_draw_scaled_bitmap(bitmap, 0, 0, original_width, original_height, 0, 0, new_width, new_height, 0); // desenha o bitmap no new_bitmap e redimensiona
-    al_set_target_bitmap(al_get_backbuffer(al_get_current_display())); // desenha na janela exibida ao usuÃ¡rio
-    al_destroy_bitmap(bitmap);
-
-    return new_bitmap;
-}
-
-void inicializa_personagem(Personagem* p, char img_direita[], char img_esquerda[], int pos_x, int pos_y, int vel, int tamanho) {
-    p->direita = carregar_imagem_redimensionada(img_direita, tamanho);
-    p->esquerda = carregar_imagem_redimensionada(img_esquerda, tamanho);
-    p->pos_x = pos_x;
-    p->pos_y = pos_y;
-    p->vel = vel;
-    p->atual = p->direita;
-    p->facing_left = false;
-}
-
-void atualiza_personagem(Personagem* p, bool key[]) {
-    if (key[0]) p->pos_y -= p->vel;  // UP
-    if (key[1]) p->pos_y += p->vel;  // DOWN
-    if (key[2]) {                    // RIGHT
-        p->pos_x += p->vel;
-        p->atual = p->direita;
-        p->facing_left = false;
-    }
-    if (key[3]) {                    // LEFT
-        p->pos_x -= p->vel;
-        p->atual = p->esquerda;
-        p->facing_left = true;
-    }
-}
-
-void desenha_personagem(Personagem* p) {
-    al_draw_bitmap(p->atual, p->pos_x, p->pos_y, 0);
-}
-
-void processa_eventos(ALLEGRO_EVENT event, bool key[], bool* running) {
-    if (event.type == ALLEGRO_EVENT_KEY_DOWN) {
-        switch (event.keyboard.keycode) {
-        case ALLEGRO_KEY_ESCAPE:
-            *running = false;
-            break;
-        case ALLEGRO_KEY_UP:
-            key[0] = true;
-            break;
-        case ALLEGRO_KEY_DOWN:
-            key[1] = true;
-            break;
-        case ALLEGRO_KEY_RIGHT:
-            key[2] = true;
-            break;
-        case ALLEGRO_KEY_LEFT:
-            key[3] = true;
-            break;
-        }
-    }
-    else if (event.type == ALLEGRO_EVENT_KEY_UP) {
-        switch (event.keyboard.keycode) {
-        case ALLEGRO_KEY_UP:
-            key[0] = false;
-            break;
-        case ALLEGRO_KEY_DOWN:
-            key[1] = false;
-            break;
-        case ALLEGRO_KEY_RIGHT:
-            key[2] = false;
-            break;
-        case ALLEGRO_KEY_LEFT:
-            key[3] = false;
-            break;
-        }
-    }
-}
-
-void inicializa_allegro(Jogo* jogo) {
-    al_init();
-    al_init_image_addon();
-    al_install_keyboard();
-
-    jogo->display = al_create_display(1000, 600);
-    jogo->timer = al_create_timer(1.0 / 60);
-    jogo->queue = al_create_event_queue();
-
-
-    al_register_event_source(jogo->queue, al_get_keyboard_event_source());
-    al_register_event_source(jogo->queue, al_get_display_event_source(jogo->display));
-    al_register_event_source(jogo->queue, al_get_timer_event_source(jogo->timer));
-
-    jogo->running = true;
-
-    al_start_timer(jogo->timer);
-}
-
-void finaliza_jogo(Jogo* jogo, Personagem* personagem, Cenario* cenario) {
-    al_destroy_display(jogo->display);
-    al_destroy_event_queue(jogo->queue);
-    al_destroy_timer(jogo->timer);
-    al_destroy_bitmap(personagem->direita);
-    al_destroy_bitmap(personagem->esquerda);
-    al_destroy_bitmap(cenario->img_bloco);
-}
-
+enum GameState {
+    menu,
+    game
+};
 
 int main() {
-    Jogo jogo;
-    Personagem personagem;
-    Cenario cenario;
-    bool key[4] = { false, false, false, false };  // UP, DOWN, RIGHT, LEFT
 
-    inicializa_allegro(&jogo);
-    inicializa_personagem(&personagem, "./img/bruxa-right.png", "./img/bruxa-left.png", 200, 200, 5, 10);
-    cenario.img_bloco = al_create_bitmap("./img/bloco.png", 200, 500);
+    al_init();
+    al_install_mouse();
+    al_init_primitives_addon();
+    al_init_font_addon();
+    al_init_image_addon();
 
+    bool running = true;
+    enum GameState state = menu;
+    int btn_x = 550, btn_y = 200, btn_w = 200, btn_h = 80; // Start game
 
-    // loop principal do jogo
-    while (jogo.running) {
+    // Display
+    ALLEGRO_DISPLAY* display = al_create_display(1280, 716);
+    ALLEGRO_BITMAP* icon = al_load_bitmap("./img/pocao.png"); // Icon do display
+    al_set_window_position(display, 200, 200);
+    al_set_window_title(display, "Monaco");
+    al_set_display_icon(display, icon);
+    ALLEGRO_FONT* font = al_create_builtin_font();
+    ALLEGRO_TIMER* timer = al_create_timer(1.0 / 60.0);
+
+    // Personagem
+    ALLEGRO_BITMAP* right;
+    ALLEGRO_BITMAP* left;
+    ALLEGRO_BITMAP* atual;
+    int pos_x, pos_y, vel;
+    bool facing_left;
+
+    ALLEGRO_BITMAP* pg = al_load_bitmap("./img/bruxa-right.png");
+    int w_original = al_get_bitmap_width(pg);
+    int h_original = al_get_bitmap_height(pg);
+    int new_w = w_original / 6;
+    int new_h = h_original / 6;
+
+    // Imagens
+    ALLEGRO_BITMAP* bg = al_load_bitmap("./img/menu-pocoes.jpg");
+    ALLEGRO_BITMAP* bg_2 = al_load_bitmap("./img/menu-2.jpg");
+
+    ALLEGRO_EVENT_QUEUE* event_queue = al_create_event_queue(); // Fila de evento
+    al_register_event_source(event_queue, al_get_display_event_source(display));
+    al_register_event_source(event_queue, al_get_timer_event_source(timer));
+    al_register_event_source(event_queue, al_get_mouse_event_source());
+    al_start_timer(timer);
+
+    while (running) {
         ALLEGRO_EVENT event;
-        al_wait_for_event(jogo.queue, &event);
-
-        // processa eventos de teclado
-        processa_eventos(event, key, &jogo.running);
-
-        // evento de fechar janela
+        al_wait_for_event(event_queue, &event);
         if (event.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
-            jogo.running = false;
+            running = false;
         }
 
-        // atualiza e desenha o personagem se o evento for do tipo TIMER
-        if (event.type == ALLEGRO_EVENT_TIMER) {
-            atualiza_personagem(&personagem, key);
+        // Estado do menu
+        if (state == menu) {
+            // Limpando a tela
             al_clear_to_color(al_map_rgb(0, 0, 0));
-            desenha_personagem(&personagem);
+
+            al_draw_bitmap(bg_2, 0, 0, 0);
+            // Desenhando botão
+            al_draw_filled_rectangle(btn_x, btn_y, btn_x + btn_w, btn_y + btn_h, al_map_rgb(162, 40, 206));
+            al_draw_text(font, al_map_rgb(255, 255, 255), btn_x + btn_w / 2, btn_y + btn_h / 4, ALLEGRO_ALIGN_CENTRE, "Start Game");
+
+            // Atualizando a tela
+            al_flip_display();
+
+            // Detectando clique no botão
+            if (event.type == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN) {
+                if (event.mouse.x >= btn_x && event.mouse.x <= btn_x + btn_w &&
+                    event.mouse.y >= btn_y && event.mouse.y <= btn_y + btn_h) {
+                    state = game;  // Mudar para o estado do jogo
+                }
+            }
+        }
+        // Estado do jogo após clicar no botão
+        else if (state == game) {
+            // Limpar a tela para o próximo estado
+            al_clear_to_color(al_map_rgb(0, 0, 0));
+
+            // Atualizar a tela
+            al_clear_to_color(al_map_rgb(105, 111, 255));
+            
+            //al_draw_bitmap(bg, 0, 0, 0);
+            al_draw_scaled_bitmap(pg, 0, 0, w_original, h_original, 20, 500, new_w, new_h, 0);
+            
             al_flip_display();
         }
+
     }
 
-    finaliza_jogo(&jogo, &personagem, &cenario);
+    al_destroy_bitmap(bg);
+    al_destroy_bitmap(bg_2);
+    al_destroy_bitmap(pg);
+    al_destroy_font(font);
+    al_destroy_display(display);
+    al_destroy_event_queue(event_queue);
+
 
     return 0;
 }
